81093a825b3b38c81b2ad88f084870a85500503b
Ahmet Emrebas <aemrebasus@gmail.com>
2020-06-18 19:22:34 -0500

    Created Circle and Rectangle drawer

--- src/app/canvas-samples/canvas-samples.component.ts
+++ src/app/canvas-samples/canvas-samples.component.ts
@@ -10,71 +10,42 @@ export class CanvasSamplesComponent implements OnInit, AfterViewInit {
 
   @ViewChild('canvas') canvas: ElementRef<HTMLCanvasElement>;
 
-  config = {
+  context: CanvasRenderingContext2D;
+
+  config: IConfig = {
     source: 'arc',
-    color: 'red',
+    style: '#00AA00',
     x: 0,
     y: 0,
     width: 0,
     height: 0,
     text: '',
+    startAngle: 0,
+    endAngle: Math.PI * 2,
+    clockwise: false,
+    radius: 30,
+
   }
 
 
   constructor() { }
   ngAfterViewInit(): void {
-
-
-
-    const context = this.canvas.nativeElement.getContext('2d');
-
-
-    // Fill reactancle
-    this.fillRect(context, {
-      style: 'red',
-      x: 0,
-      y: 0,
-      width: 300,
-      height: 300
-    });
-
-    // Fill reactancle
-    this.fillRect(context, {
-      style: 'blue',
-      x: 50,
-      y: 50,
-      width: 20,
-      height: 20
-    });
-
-
-    // Fill Arc
-    this.fillArc(context, {
-      x: 100,
-      y: 100,
-      startAngle: 0,
-      endAngle: (Math.PI),
-      radius: 30,
-      style: 'blue',
-      clockwise: false
-    });
-
-
+    this.context = this.canvas.nativeElement.getContext('2d');
   }
 
 
 
 
-  fillRect(context: CanvasRenderingContext2D, area: IRect) {
+  fillRect(area: IRect) {
     const { style, x, y, width, height } = area;
-    context.fillStyle = style;
-    context.fillRect(x, y, width, height);
+    this.context.fillStyle = style;
+    this.context.fillRect(x, y, width, height);
   }
 
 
-  fillArc(context: CanvasRenderingContext2D, arc: IArc) {
-    context.arc(arc.x, arc.y, arc.radius, arc.startAngle, arc.endAngle, arc.clockwise);
-    context.fill();
+  fillArc(arc: IArc) {
+    this.context.arc(arc.x, arc.y, arc.radius, arc.startAngle, arc.endAngle, arc.clockwise);
+    this.context.fill();
   }
 
 
@@ -85,30 +56,64 @@ export class CanvasSamplesComponent implements OnInit, AfterViewInit {
 
 
   render(): void {
+    switch (this.config.source) {
 
+      case 'arc':
+        this.context.fillStyle = this.config.style;
+
+        this.context.beginPath();
+        this.context.arc(this.config.x, this.config.y, this.config.radius,
+          this.config.startAngle, this.config.endAngle, this.config.clockwise);
+
+        if (this.config.stroke) {
+          this.context.lineWidth = this.config.lineWidth;
+          this.context.strokeStyle = this.config.strokeStyle;
+          this.context.stroke();
+        }
+        this.context.fill();
+        this.context.closePath();
+
+        break;
+
+      case 'rect':
+        this.context.beginPath();
+        this.context.fillStyle = this.config.style;
+        this.context.fillRect(this.config.x, this.config.y, this.config.width, this.config.height);
+        this.context.closePath();
+        break;
+    }
 
   }
 
   clear(): void {
+    this.context.clearRect(0, 0, 200, 200);
 
   }
 
 
 }
+export interface IConfig extends IRect, IArc {
+  source?: 'arc' | 'rect' | 'img' | 'video';
+  text?: string;
+}
+
 export interface IShape {
-  style: string;
-  x: number;
-  y: number;
+  style?: string;
+  x?: number;
+  y?: number;
+  stroke?: boolean;
+  strokeStyle?: string;
+  lineWidth?: number;
 }
 
 export interface IRect extends IShape {
-  width: number;
-  height: number;
+  width?: number;
+  height?: number;
 }
 
 export interface IArc extends IShape {
-  radius: number;
-  startAngle: number;
-  endAngle: number;
-  clockwise: boolean;
+  radius?: number;
+  startAngle?: number;
+  endAngle?: number;
+  clockwise?: boolean;
 }
